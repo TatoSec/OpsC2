37d94e4d63c9460873678329a084674e042af1d1
Ivan Fonseca <107064877+TatoSec@users.noreply.github.com>
2023-10-24 09:07:10 -0400

    UI Update c2.py

--- c2.py
+++ c2.py
@@ -1,10 +1,30 @@
 import subprocess
+import time
 
 # ANSI escape codes for text formatting
 BOLD = "\033[1m"
 RESET = "\033[0m"
 BLUE = "\033[34m"
 GREEN = "\033[32m"
+
+# UI Hacker Text
+def hacker_print(text, leading_spaces = 0):
+
+	text_chars = list(text)
+	current, mutated = '', ''
+
+	for i in range(len(text)):
+		
+		original = text_chars[i]
+		current += original
+		mutated += f'\033[1;38;5;82m{text_chars[i].upper()}\033[0m'
+		print(f'\r{" " * leading_spaces}{mutated}', end = '')
+		time.sleep(0.05)
+		print(f'\r{" " * leading_spaces}{current}', end = '')
+		mutated = current
+
+	print(f'\r{" " * leading_spaces}{text}\n')
+
 
 # Define your ASCII art banner
 banner = r"""
@@ -29,6 +49,6 @@ def nc_listener():
 
 
 print(banner)
-print("Listening")
+hacker_print(f"{GREEN}Initiating listener..")
 nc_listener()
 
